dae9d31c018502a6e1803b7ad91e777a8055ba86
KieranHarkin <kieranharkin@googlemail.com>
2017-05-27 00:36:24 +0100

    Configuration Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -12,7 +13,13 @@ import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   imports: [ BrowserModule,
-  FormsModule, HttpModule ],
+  FormsModule, HttpModule, RouterModule.forRoot([
+    { path: 'products', component: ProductListComponent },
+    { path: 'products/:id', component: ProductDetailComponent },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+  ]) ],
   declarations: [ AppComponent, ProductListComponent,
   ProductFilterPipe, StarComponent, ProductDetailComponent, WelcomeComponent ],
   bootstrap: [ AppComponent ]
