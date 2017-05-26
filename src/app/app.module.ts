1f4a2c3f5f3bf2290ee286868023a7444fdf319f
KieranHarkin <kieranharkin@googlemail.com>
2017-05-26 23:03:15 +0100

    Adding product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductFilterPipe } from './products/product-filter.Pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   imports: [ BrowserModule,
   FormsModule, HttpModule ],
   declarations: [ AppComponent, ProductListComponent,
-  ProductFilterPipe, StarComponent ],
+  ProductFilterPipe, StarComponent, ProductDetailComponent, WelcomeComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
